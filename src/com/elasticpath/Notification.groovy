8c9cf7cefc5a851d8090ec0ba6e1f88ee4e3d7bb
Mindaugas Vosylius <mindaugas.vosylius@elasticpath.com>
2019-03-25 18:20:04 +0200

    testing

--- src/com/elasticpath/Notification.groovy
+++ src/com/elasticpath/Notification.groovy
@@ -4,10 +4,10 @@ class Notification {
 
 	static def sendSlackNotification(script, buildStatus, color) {
 		def userMention = BUILD_USER_SLACK_ID != "" ? "<@${script.env.BUILD_USER_SLACK_ID}>" : ""
-		sendSlackNotification(buildStatus, color, userMention)
+		sendSlackNotificationInternal(buildStatus, color, userMention)
 	}
 
-	static def sendSlackNotification(script, buildStatus, color, userMention) {
+	static def sendSlackNotificationInternal(script, buildStatus, color, userMention) {
 		slackSend color: color,
 				message: "${script.env.JOB_NAME} - #${script.env.BUILD_NUMBER} ($script.env.BRANCH_NAME_NO_SPACES) " + buildStatus + ", " + "${script.env.currentBuild.durationString.replace(' and counting', '')} (<${BUILD_URL}|Link>) " + userMention
 	}
