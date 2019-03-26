4fc6619bb65f13de237971912805a2c734d46519
Mindaugas Vosylius <mindaugas.vosylius@elasticpath.com>
2019-03-26 14:15:48 +0200

    test

--- vars/githubStatusSetter.groovy
+++ vars/githubStatusSetter.groovy
@@ -1,10 +1,11 @@
 
 void setBuildStatus(def script, String message, String state) {
+	def repositoryUrl = scm.userRemoteConfigs[0].url
 	script.timeout(time: 1, unit: 'MINUTES') {
 		script.retry(3) {
 			script.step([
 					$class            : "GitHubCommitStatusSetter",
-					reposSource       : [$class: "ManuallyEnteredRepositorySource", url: "git@github.com:ep-pd/autobahn-ep-commerce.git"],
+					reposSource       : [$class: "ManuallyEnteredRepositorySource", url: "$repositoryUrl"],
 					contextSource     : [$class: "ManuallyEnteredCommitContextSource", context: "${script.GITHUB_COMMIT_STATUS_CONTEXT}"],
 					backrefSource     : [$class: "ManuallyEnteredBackrefSource", backref: "${script.env.BUILD_URL}"],
 					statusResultSource: [$class: "ConditionalStatusResultSource", results: [[$class: "AnyBuildResult", message: message, state: state]]]
