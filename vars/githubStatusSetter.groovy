2a5752c06dd79c76453f665d57e4e256ab85caf0
Mindaugas Vosylius <mindaugas.vosylius@elasticpath.com>
2019-03-26 13:19:02 +0200

    testing

--- vars/githubStatusSetter.groovy
+++ vars/githubStatusSetter.groovy
@@ -1,11 +1,11 @@
 
-void setBuildStatus(def script, String buildUrl, String context, String message, String state) {
-	script.timeout(time: 1, unit: 'MINUTES') {
-		script.retry(3) {
-			script.step([
+void setBuildStatus(def script, String message, String state) {
+	timeout(time: 1, unit: 'MINUTES') {
+		retry(3) {
+			step([
 					$class            : "GitHubCommitStatusSetter",
-					contextSource     : [$class: "ManuallyEnteredCommitContextSource", context: context],
-					backrefSource     : [$class: "ManuallyEnteredBackrefSource", backref: buildUrl],
+					contextSource     : [$class: "ManuallyEnteredCommitContextSource", context: "${script.GITHUB_COMMIT_STATUS_CONTEXT}"],
+					backrefSource     : [$class: "ManuallyEnteredBackrefSource", backref: "${script.env.BUILD_URL}"],
 					statusResultSource: [$class: "ConditionalStatusResultSource", results: [[$class: "AnyBuildResult", message: message, state: state]]]
 			]);
 		}
