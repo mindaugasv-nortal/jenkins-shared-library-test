9c15193d1fd905cb9c8eaa4b63f07ff647250e9c
Mindaugas Vosylius <mindaugas.vosylius@elasticpath.com>
2019-03-26 14:03:23 +0200

    test

--- vars/githubStatusSetter.groovy
+++ vars/githubStatusSetter.groovy
@@ -4,6 +4,7 @@ void setBuildStatus(def script, String message, String state) {
 		script.retry(3) {
 			script.step([
 					$class            : "GitHubCommitStatusSetter",
+					reposSource       : [$class: "ManuallyEnteredRepositorySource", context: "git@github.com:ep-pd/autobahn-ep-commerce.git"],
 					contextSource     : [$class: "ManuallyEnteredCommitContextSource", context: "${script.GITHUB_COMMIT_STATUS_CONTEXT}"],
 					backrefSource     : [$class: "ManuallyEnteredBackrefSource", backref: "${script.env.BUILD_URL}"],
 					statusResultSource: [$class: "ConditionalStatusResultSource", results: [[$class: "AnyBuildResult", message: message, state: state]]]
